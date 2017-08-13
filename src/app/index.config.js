a0504fd1f55199239ccb59a6df59fd9d06853586
Levin Calado <levincalado@gmail.com>
2017-08-13 16:12:39 +0800

    config: changed toastrConfig and added mdThemingProviders

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,16 +6,22 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig) {
+  function config($logProvider, toastrConfig, $mdThemingProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
 
     // Set options third-party lib
     toastrConfig.allowHtml = true;
     toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
+    toastrConfig.positionClass = 'toast-bottom-right';
     toastrConfig.preventDuplicates = true;
     toastrConfig.progressBar = true;
+    toastrConfig.closeButton = true;
+
+    $mdThemingProvider.theme('light-green').backgroundPalette('green').accentPalette('blue');
+    $mdThemingProvider.theme('light-blue').backgroundPalette('blue').accentPalette('amber');
+    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').accentPalette('amber').dark();
+    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
   }
 
 })();

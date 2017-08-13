ede249a21a92ca94291251f7fc4af2b2106fcded
Levin Calado <levincalado@gmail.com>
2017-08-13 16:11:48 +0800

    route: add initial routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,9 +8,27 @@
   function routeConfig($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
+        redirectTo: '/home'
+      })
+      .when('/home', {
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .when('/resume', {
+        templateUrl: 'app/resume/resume.html',
+        controller: 'ResumeController',
+        controllerAs: 'resume'
+      })
+      .when('/portfolio', {
+        templateUrl: 'app/portfolio/portfolio.html',
+        controller: 'PortfolioController',
+        controllerAs: 'portfolio'
+      })
+      .when('/contact', {
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactController',
+        controllerAs: 'contact'
       })
       .otherwise({
         redirectTo: '/'

17a2bae123326d841de8ba114dd73bbc9c8a7c54
Levin Calado <levincalado@gmail.com>
2017-08-19 20:54:29 +0800

    gulp/build.js: add font-awesome on fonts task

--- gulp/build.js
+++ gulp/build.js
@@ -51,6 +51,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(cssFilter)
     // .pipe($.sourcemaps.init())
     .pipe($.replace('../../bower_components/material-design-iconfont/iconfont/', '../fonts/'))
+    .pipe($.replace('../../bower_components/components-font-awesome/fonts/', '../fonts/'))
     .pipe($.cssnano())
     .pipe($.rev())
     // .pipe($.sourcemaps.write('maps'))
@@ -71,7 +72,10 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles().concat('bower_components/material-design-iconfont/iconfont/*'))
+  return gulp.src([
+        'bower_components/material-design-iconfont/iconfont/*',
+        'bower_components/components-font-awesome/fonts/*'
+      ])
     .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));

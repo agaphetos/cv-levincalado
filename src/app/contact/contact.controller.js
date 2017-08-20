87a9bffdf5559e2336ea81e2ceb0107b31795386
Levin Calado <levincalado@gmail.com>
2017-08-20 10:55:13 +0800

    contact: inital contact form

--- src/app/contact/contact.controller.js
+++ src/app/contact/contact.controller.js
@@ -5,14 +5,47 @@
         .module('cvApp')
         .controller('ContactController', ContactController);
 
-    ContactController.$inject = [];
+    ContactController.$inject = ["toastr", "$timeout", "$location"];
 
     /** @ngInject */
-    function ContactController() {
+    function ContactController(toastr, $timeout, $location) {
         var vm = this;
+
+        vm.currentDate = new Date();
+        vm.message = {};
+
+        vm.submit = submit;
+        vm.reset = reset;
 
         init(); 
 
-        function init() {}
+        function init() {
+            initMessage();
+        }
+
+        function initMessage() {
+            vm.message = {
+                'date': vm.currentDate,
+                'name': '',
+                'email': '',
+                'content': ''
+            };
+        }
+
+        function submit() {
+            if(!vm.form.$invalid) {
+                console.log(JSON.stringify(vm.message));
+                toastr.info('Message sent! Please wait for my response in your email. Thanks ' + vm.name + '. Redirecting to home page.');
+                vm.classAnimation = '';
+                $timeout(function(){
+                    $location.path("/");
+                }, 2000);
+            }
+        }
+
+        function reset() {
+            initMessage();
+            vm.form.$setPristine();
+        }
     }
 })();
